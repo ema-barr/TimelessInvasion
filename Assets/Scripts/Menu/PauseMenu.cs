d36f2105918b3c8c8fa200153e4de1b437e2ea7e
ema-barr <emanuele.barracchia@hotmail.it>
2019-04-20 20:09:28 +0200

    Implemented Main Menu and Pause Menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+
+    [SerializeField]
+    private GameObject pausePanel;
+    [SerializeField]
+    private string mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            ChangePause();
+        }
+
+        
+    }
+
+    public void Resume()
+    {
+        ChangePause();
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    private void ChangePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
