c935dd7e8a57456e8236ab6a5f6195fdb748d738
ema-barr <emanuele.barracchia@hotmail.it>
2019-04-26 17:15:38 +0200

    Added ToBeContinued

---
+++ Assets/Scripts/ToBeContinuedPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ToBeContinuedPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject toBeCondinuedText;
+    [SerializeField]
+    private string mainMenuScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowToBeContinuedCo());
+    }
+
+    private IEnumerator ShowToBeContinuedCo()
+    {
+        yield return new WaitForSeconds(3f);
+        toBeCondinuedText.SetActive(true);
+
+        yield return new WaitForSeconds(4f);
+
+        SceneManager.LoadScene(mainMenuScene);
+
+    }
+}
